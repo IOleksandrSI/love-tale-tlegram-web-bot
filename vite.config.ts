6c93282ac12502acf6b3f5d5036b99d0104bc106
ShadowSoilders <sasha.struk1@gmail.com>
2025-01-26 20:24:47 +0000

    t1

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default ((config: ConfigEnv) => {
   const { mode } = config;
-
-  console.log(process);
-
+  
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
   return defineConfig({
